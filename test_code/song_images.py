201fd705aaa6f434382cd44c5c8e4553153f6898
aldrin <aldrin.gordon@outlook.com>
2020-04-29 13:21:48 -0600

    cleaned up code for frame getter, start on img diff

--- test_code/song_images.py
+++ test_code/song_images.py
@@ -1,16 +1,37 @@
 import cv2
 
+# for image compare
+import numpy as np
+from skimage.measure import compare_ssim as ssim
+
+# ImageCompare: returns true if 2 images are the same
+# uses: cv2, numpy, sckit
+####################################################################################
+
+
+
+
+# Gets frames of song
+# uses: cv2, ImageCompare
+####################################################################################
+
 vidcap = cv2.VideoCapture('../test_data/test.mp4')
 frames = []
+
+# save frames as JPG
+def writeFrames(image, path):
+    cv2.imwrite(path, image)
+
+def saveFrames(image):
+    frames.append(image)
 
 def getFrame(sec):
     vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
     hasFrames,image = vidcap.read()
     if hasFrames:
-        frames.append(image)
-#        cv2.imwrite("frames/image"+str(count)+".jpg", image)     # save frame as JPG file
+        writeFrames(image, "frames/image"+str(count)+".jpg")
+        saveFrames(image) # append to list
     return hasFrames
-
 
 sec = 0
 frameRate = 0.1 #//it will capture image in each 0.2 second
@@ -26,8 +47,4 @@ while i < len(frames):
     if frames[i] == frames[i-1]:
         frames.pop(i)
 
-count = 1
-for image in frames:
-    cv2.imwrite("frames/image"+str(count)+".jpg", image)
-    count += 1
 
