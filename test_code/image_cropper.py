7f7958afdb1c1c1f0611a638a2d6e19d762d1332
aldrin <aldrin.gordon@outlook.com>
2020-04-30 10:48:33 -0600

    reformat, set import/export image folders

--- test_code/image_cropper.py
+++ test_code/image_cropper.py
@@ -2,21 +2,41 @@
 # crop each image so that it only has the song name-artist
 # write each image
 
-# Reference: https://www.geeksforgeeks.org/python-pil-image-crop-method/
+# References:
+    # https://www.geeksforgeeks.org/python-pil-image-crop-method/
+    # https://mkyong.com/python/python-how-to-list-all-files-in-a-directory/
+
+import os
 
 # Python Image Library's Image Class
 from PIL import Image
 
-# Opens RGB Image
-im = Image.open("crop/test.jpg")
+directory = "crop/"
+import_folder = "to_crop/"
+export_folder = "cropped/"
 
-# Points for Cropped Image
-left = 0
-top = 650
-right = 600
-bottom = 800
+path = directory+import_folder
 
-# Crop Image
-cr = im.crop((left, top, right, bottom))
+files = []
+# r=root, d=directories, f=files
+for r, d, f in os.walk(path):
+    for file in f:
+        if ".jpg" in file:
+            files.append(os.path.join(r, file))
 
-cr.show()
+for f in files:
+    
+    # Opens RGB Image
+    im = Image.open(f)
+
+    # Points for Cropped Image
+    left = 0
+    top = 650
+    right = 600
+    bottom = 800
+
+    # Crop Image
+    cr = im.crop((left, top, right, bottom))
+
+    #cr.show()
+    cr.save(f.replace(import_folder, export_folder))
