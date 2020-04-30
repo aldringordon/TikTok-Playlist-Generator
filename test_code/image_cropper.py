47fd7e320d24a89464ae5fc9bdc283bb7251a928
aldrin <aldrin.gordon@outlook.com>
2020-04-30 10:31:01 -0600

    image crops fine with "RUFUS" as test image

--- test_code/image_cropper.py
+++ test_code/image_cropper.py
@@ -1,3 +1,22 @@
 # take all the image files from the template matched folder
 # crop each image so that it only has the song name-artist
 # write each image
+
+# Reference: https://www.geeksforgeeks.org/python-pil-image-crop-method/
+
+# Python Image Library's Image Class
+from PIL import Image
+
+# Opens RGB Image
+im = Image.open("crop/test.jpg")
+
+# Points for Cropped Image
+left = 0
+top = 650
+right = 600
+bottom = 800
+
+# Crop Image
+cr = im.crop((left, top, right, bottom))
+
+cr.show()
